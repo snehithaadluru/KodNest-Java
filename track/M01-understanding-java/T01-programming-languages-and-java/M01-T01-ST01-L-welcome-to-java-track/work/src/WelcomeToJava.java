1d721114ea770503add8858ef8c19f452d15d00a
snehithaadluru <snehithaadluru@gmail.com>
2026-09-01 12:48:58 +0530

    I have written my first code

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,7 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
+        System.err.println("");
+        System.err.println("I am in Java Track");
        //Write your code here
     }
 }
